fe2ec9bb934da0a687c89ac627d46ee4bd2425ba
Konsta Purtsi <konsta.purtsi@vincit.fi>
2018-10-08 14:12:48 +0300

    Updates

---
+++ precache-manifest.0063027d940659a2aa978da7e8718e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/juomamaatti-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7f5e3e55d1e39295b54",
+    "url": "/juomamaatti-react/static/js/runtime~main.d7f5e3e5.js"
+  },
+  {
+    "revision": "a095b6dd2dc0958c9b18",
+    "url": "/juomamaatti-react/static/js/main.a095b6dd.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/juomamaatti-react/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a095b6dd2dc0958c9b18",
+    "url": "/juomamaatti-react/static/css/main.c5d84f35.chunk.css"
+  },
+  {
+    "revision": "21716c35407f3b3f4e874e1df5c723fc",
+    "url": "/juomamaatti-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/juomamaatti/precache-manifest.a251acb30097b72e61bfdb6c7e9dbe47.js"
+  "/juomamaatti-react/precache-manifest.0063027d940659a2aa978da7e8718e91.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/juomamaatti/index.html", {
+workbox.routing.registerNavigationRoute("/juomamaatti-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
