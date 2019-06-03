a234866470015e6ac9dbb656f337ef974dd260e7
Konsta Purtsi <kovipu@utu.fi>
2019-06-03 22:49:35 +0300

    Updates

---
+++ precache-manifest.16872a36e7769f6e0c80a35d34cc9cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3008a5bc2c59c768964095f7501742b",
+    "url": "/juomamaatti-react/index.html"
+  },
+  {
+    "revision": "584b4a5dd2a8c147d824",
+    "url": "/juomamaatti-react/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "dc12c79803a4fcfd6a27",
+    "url": "/juomamaatti-react/static/js/2.5415f1c5.chunk.js"
+  },
+  {
+    "revision": "584b4a5dd2a8c147d824",
+    "url": "/juomamaatti-react/static/js/main.b3ca3184.chunk.js"
+  },
+  {
+    "revision": "249825985d8729ece3a2",
+    "url": "/juomamaatti-react/static/js/runtime~main.56a1eb74.js"
+  }
+]);
